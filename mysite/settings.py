b66836123f3387c280930816e49c1729ebb6cdd0
Howard Edson <howard.edson@gmail.com>
2019-07-20 11:27:42 -0700

    add .ebextensions and aws config

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = get_secret('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.w2g9guwbah.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
